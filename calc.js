d00bf36300aaad199c6f1d971b27bc4b29cc1c51
Josh Cunningham <josh@joshcanhelp.com>
2015-10-14 05:57:58 -0700

    proof or percent determined by type radio, stops calculation if a critical value is missing, more clear cost calculation

--- calc.js
+++ calc.js
@@ -1,5 +1,7 @@
+/* global console, alert */
+
 jQuery(document).ready( function ($) {
-	"use strict";
+	'use strict';
 
 	$('.clear_results').click(function () {
 		$('#' + $(this).attr('data-rel')).html('');
@@ -7,35 +9,89 @@ jQuery(document).ready( function ($) {
 
 	$('input[name=type]').change(function () {
 
+		$('[for="proof"]').text(
+			$(this).attr('id') === 'type_spirit' ?
+				'Proof' :
+				'ABV %'
+		);
+
 	});
 
 	$('#drink_cost').submit(function (e) {
 		e.preventDefault();
 
-		var proof = $(this).find('#proof').val();
-		var volume = $(this).find('#volume').val();
-		var cost = $(this).find('#cost').val();
-		var costPerDrink = calc_drink_cost(proof, volume, cost);
+		// Cached selectors
 
-		if (isNaN(costPerDrink)) {
-			alert('Cannot calc!');
+		var thisForm = $(this);
+		var percentEl = $('#proof');
+		var volumeEl = $('#volume');
+		var costEl = $('#cost');
+
+		var percent = parseInt(percentEl.val(), 10);
+		var type = thisForm.find('[name="type"]:checked').val();
+
+		if (!percent) {
+			alert('Need a ' + $('[for="proof"]').text());
 			return;
 		}
 
+		// Cost calc below expects percent
+		// Converts proof to percent
+
+		if (type === 'spirit') {
+			percent = percent / 2;
+		}
+
+		var volume = parseInt(volumeEl.val(), 10);
+		var units = thisForm.find('[name="units"]:checked').val();
+
+		if (!volume) {
+			alert('Need a volume');
+			return;
+		}
+
+		// Cost calc below expects mL
+		// Converts from ounces to mL
+
+		if (units === 'oz') {
+			volume = volume * 29.5735;
+		}
+
+		var cost = costEl.val();
+
+		if (!cost) {
+			alert('Need a cost');
+			return;
+		}
+
+		var costPerDrink = calcDrinkCost(percent, volume, cost);
+
 		$('#drink_cost_rows')
 			.append($('<tr/>')
-				.append($('<td/>').text(proof))
-				.append($('<td/>').text(volume))
-				.append($('<td/>').text(cost))
-				.append($('<td/>').text(Math.round(costPerDrink * 100) / 100))
+				.append($('<td/>').text(percent + '%'))
+				.append($('<td/>').text(Math.round(volume * 10) / 10 + ' mL'))
+				.append($('<td/>').text('$' + cost))
+				.append($('<td/>').text('$' + Math.round(costPerDrink * 100) / 100))
 			);
+
+		// Clear out fields
+		percentEl.val('');
+		volumeEl.val('');
+		costEl.val('');
 
 	});
 
 });
 
-function calc_drink_cost(p, v, c) {
-	"use strict";
+function calcDrinkCost(p, v, c) {
+	'use strict';
 
-	return ( c * 1.205 + ( ( v / 1000 ) * 3.7708 ) ) / ( ( ( p / 200 ) / 0.4 ) * v * 0.033814 );
+	// USA standard drink is 1.4mL of pure ethanol
+	// https://en.wikipedia.org/wiki/Standard_drink#Defined_by_various_countries
+
+	var percentAlc = ( p / 100 );
+	var totalAlc = v * percentAlc;
+	var totalDrinks = totalAlc / 17.7;
+
+	return c / totalDrinks;
 }
